f3f8a07773731a27ad06e4846fd6d375fb6ba65c
Eyeroniq <133900191+Eyeroniq@users.noreply.github.com>
2023-10-15 18:30:11 +0530

    Add files via upload

---
+++ bnbdevs/js/js.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(".whoPart1").waypoint(function(){
+     $(".whoPart1").addClass("animate__animated animate__fadeInLeft");
+    },{offset:"80%"} ); });
+
+    $(document).ready(function(){
+        $(".do1").waypoint(function(){
+         $(".do1").addClass("animate__animated animate__fadeInUp");
+        },{offset:"80%"} ); });
+    
+    $(document).ready(function(){
+            $(".whoPart2").waypoint(function(){
+             $(".whoPart2").addClass("animate__animated animate__fadeInRight");
+            },{offset:"80%"} ); });
+        
+            $(document).ready(function(){
+                $(".flip").waypoint(function(){
+                 $(".flip").addClass("animate__animated animate__fadeInUp");
+                },{offset:"80%"} ); });
+
+ $(document).ready(function(){
+          $(".contA").waypoint(function(){
+              $(".contA").addClass("animate__animated animate__fadeInUpBig");
+                    },{offset:"80%"} ); });
+
+$(document).ready(function(){
+    $(".portA").waypoint(function(){
+    $(".portA").addClass("animate__animated animate__fadeInUpBig");
+    },{offset:"80%"} ); });
+
+
+    $(document).ready(function(){
+        $(".trustA").waypoint(function(){
+        $(".trustA").addClass("animate__animated animate__fadeInUp");
+        },{offset:"90%"} ); });
